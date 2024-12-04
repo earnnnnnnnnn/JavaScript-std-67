bede1af9647e7757d8f62cb2fb6b4807c99d7521
eannnnnnnnn <s6606021631017@email.kmutnb.ac.th>
2024-12-04 09:59:17 +0700

    2

--- Lecture_02/case-sensitive.js
+++ Lecture_02/case-sensitive.js
@@ -1,7 +1,7 @@
 const status = 'error';
 
 if (statuss.toUpperCase() === 'ERROR'){
-    console,log('Something went wronga!');
+    console.log('Something went wronga!');
 }else{
     console.log('Looks great!!');
 }
